5ca8676007c0ae0f77353725e25df5f77f78ef7c
Joseph Sutera <joesutera11@gmail.com>
2022-07-15 20:04:58 -0400

    commit tree 984000ce83c70021ba44a885a53b291e650f6c29

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(array){
+    const winner = array.find(item=>item.result === 'W');
+    if(!!winner){
+       return winner.year 
+    }else{
+     return undefined
+     }
+    
+}
+
+  
+ 
+  
